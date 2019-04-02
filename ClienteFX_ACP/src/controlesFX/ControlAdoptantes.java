c9d042ca8601560a965e6b134b2939c6dbe443de
rguerreroayon <rguerreroayon@gmail.com>
2019-04-02 04:43:06 -0700

    Started JavaFX Project

---
+++ ClienteFX_ACP/src/controlesFX/ControlAdoptantes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlesFX;
+
+import accesoDatos.DAOAdoptantes;
+
+/**
+ *
+ * @author roberto
+ */
+public class ControlAdoptantes {
+
+    DAOAdoptantes adoptantes = new DAOAdoptantes();
+}
---
+++ ClienteFX_ACP/src/controlesFX/ControlAnimales.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlesFX;
+
+import accesoDatos.DAOAnimales;
+
+/**
+ *
+ * @author roberto
+ */
+public class ControlAnimales {
+
+    DAOAnimales animales = new DAOAnimales();
+
+}

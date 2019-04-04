14cd56e77c7c1b017cf5c0dcc4a2e526c8a005a1
Roberto Guerrero <rguerreroayon@gmail.com>
2019-04-04 03:11:40 -0700

    Switched ClienteFX project from Netbeans to GLORIOUS IntelliJ

---
+++ ClienteFX_ACP_IntelliJ/src/controles/controlesDatos/ControlAdoptantes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controles.controlesDatos;
+
+import accesoDatos.DAOAdoptantes;
+
+/**
+ *
+ * @author roberto
+ */
+public class ControlAdoptantes {
+
+    DAOAdoptantes adoptantes;
+
+    public ControlAdoptantes() {
+        adoptantes = new DAOAdoptantes();
+    }
+
+    public DAOAdoptantes getAdoptantes() {
+        return adoptantes;
+    }
+    
+    
+}
---
+++ ClienteFX_ACP_IntelliJ/src/controles/controlesDatos/ControlVoluntarios.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controles.controlesDatos;
+
+import accesoDatos.DAOVoluntarios;
+
+/**
+ *
+ * @author roberto
+ */
+public class ControlVoluntarios {
+
+    DAOVoluntarios voluntarios;
+    
+    public ControlVoluntarios(){
+        voluntarios = new DAOVoluntarios();
+    }
+
+    public DAOVoluntarios getVoluntarios() {
+        return voluntarios;
+    }
+    
+    
+    
+
+}
